14743c1fcccde3d5c3801112420a271207f3ccf6
Sandy Lerman <alex@ablerman.com>
2018-10-03 19:56:27 -0400

    layout

--- src/constants/musicPhotos.js
+++ src/constants/musicPhotos.js
@@ -1,283 +1,370 @@
 const photos = [
     {
-        path: '/portfolio/music/JoeyBadass.jpg',
-        alt: 'JoeyBadass'
-    },
-    {
-        path: '/portfolio/music/actionBronson.jpg',
-        alt: 'actionBronson'
-    },
-    {
-        path: '/portfolio/music/aluna.jpg',
-        alt: 'aluna'
-    },
-    {
-        path: '/portfolio/music/andyC.jpg',
-        alt: 'andyC'
-    },
-    {
-        path: '/portfolio/music/asap.jpg',
-        alt: 'asap'
-    },
-    {
-        path: '/portfolio/music/baauer.jpg',
-        alt: 'baauer'
-    },
-    {
-        path: '/portfolio/music/bar.jpg',
-        alt: 'bar'
-    },
-    {
-        path: '/portfolio/music/bicep.jpg',
-        alt: 'bicep'
-    },
-    {
-        path: '/portfolio/music/charlie.jpg',
-        alt: 'charlie'
-    },
-    {
-        path: '/portfolio/music/damien.jpg',
-        alt: 'damien'
-    },
-    {
-        path: '/portfolio/music/dangelo.jpg',
-        alt: 'dangelo'
-    },
-    {
-        path: '/portfolio/music/daniel.jpg',
-        alt: 'daniel'
-    },
-    {
-        path: '/portfolio/music/darius.jpg',
-        alt: 'darius'
-    },
-    {
-        path: '/portfolio/music/daughter.jpg',
-        alt: 'daughter'
-    },
-    {
-        path: '/portfolio/music/dimensions.jpg',
-        alt: 'dimensions'
-    },
-    {
-        path: '/portfolio/music/dj.jpg',
-        alt: 'dj'
+        path: '/portfolio/music/jurassicPortrait.jpg',
+        alt: 'jurassicPortrait',
+        colSpan: 4,
+        rowSpan: 3,
     },
     {
         path: '/portfolio/music/djEZ.jpg',
-        alt: 'djEZ'
-    },
-    {
-        path: '/portfolio/music/ellesseGirl.jpg',
-        alt: 'ellesseGirl'
-    },
-    {
-        path: '/portfolio/music/ellie.jpg',
-        alt: 'ellie'
-    },
-    {
-        path: '/portfolio/music/evolutionCrowd.jpg',
-        alt: 'evolutionCrowd'
-    },
-    {
-        path: '/portfolio/music/example.jpg',
-        alt: 'example'
-    },
-    {
-        path: '/portfolio/music/fanGirl.jpg',
-        alt: 'fanGirl'
-    },
-    {
-        path: '/portfolio/music/flava.jpg',
-        alt: 'flava'
-    },
-    {
-        path: '/portfolio/music/frank.jpg',
-        alt: 'frank'
-    },
-    {
-        path: '/portfolio/music/guitar.jpg',
-        alt: 'guitar'
-    },
-    {
-        path: '/portfolio/music/gunshots.jpg',
-        alt: 'gunshots'
-    },
-    {
-        path: '/portfolio/music/hands.jpg',
-        alt: 'hands'
-    },
-    {
-        path: '/portfolio/music/handshake.jpg',
-        alt: 'handshake'
-    },
-    {
-        path: '/portfolio/music/heidi.jpg',
-        alt: 'heidi'
-    },
-    {
-        path: '/portfolio/music/jake.jpg',
-        alt: 'jake'
-    },
-    {
-        path: '/portfolio/music/jamBaxter.jpg',
-        alt: 'jamBaxter'
-    },
-    {
-        path: '/portfolio/music/joey.jpg',
-        alt: 'joey'
-    },
-    {
-        path: '/portfolio/music/jonHopkins.jpg',
-        alt: 'jonHopkins'
-    },
-    {
-        path: '/portfolio/music/joyOrbison.jpg',
-        alt: 'joyOrbison'
-    },
-    {
-        path: '/portfolio/music/jrocc.jpg',
-        alt: 'jrocc'
-    },
-    {
-        path: '/portfolio/music/jurassic5.jpg',
-        alt: 'jurassic5'
-    },
-    {
-        path: '/portfolio/music/jurassicPortrait.jpg',
-        alt: 'jurassicPortrait'
-    },
-    {
-        path: '/portfolio/music/kelis.jpg',
-        alt: 'kelis'
-    },
-    {
-        path: '/portfolio/music/kelisPortrait.jpg',
-        alt: 'kelisPortrait'
-    },
-    {
-        path: '/portfolio/music/killerMike.jpg',
-        alt: 'killerMike'
-    },
-    {
-        path: '/portfolio/music/loveboxCrowd.jpg',
-        alt: 'loveboxCrowd'
-    },
-    {
-        path: '/portfolio/music/lunice.jpg',
-        alt: 'lunice'
-    },
-    {
-        path: '/portfolio/music/moat.jpg',
-        alt: 'moat'
-    },
-    {
-        path: '/portfolio/music/nozstockCrowd.jpg',
-        alt: 'nozstockCrowd'
-    },
-    {
-        path: '/portfolio/music/nozstockThree.jpg',
-        alt: 'nozstockThree'
-    },
-    {
-        path: '/portfolio/music/oceanWisdom.jpg',
-        alt: 'oceanWisdom'
+        alt: 'djEZ',
+        colSpan: 4,
     },
     {
         path: '/portfolio/music/onemanHideout.jpg',
-        alt: 'onemanHideout'
+        alt: 'onemanHideout',
+        colSpan: 4,
     },
     {
-        path: '/portfolio/music/paak.jpg',
-        alt: 'paak'
+        path: '/portfolio/music/frank.jpg',
+        alt: 'frank',
+        colSpan: 8,
+        rowSpan: 2,
     },
     {
-        path: '/portfolio/music/paloma.jpg',
-        alt: 'paloma'
+        path: '/portfolio/music/ellesseGirl.jpg',
+        alt: 'ellesseGirl',
+        colSpan: 4,
     },
     {
-        path: '/portfolio/music/parklifeCrowd.jpg',
-        alt: 'parklifeCrowd'
+        path: '/portfolio/music/weAreCrowd.jpg',
+        alt: 'weAreCrowd',
+        colSpan: 4,
     },
     {
-        path: '/portfolio/music/planB.jpg',
-        alt: 'planB'
+        path: '/portfolio/music/fanGirl.jpg',
+        alt: 'fanGirl',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/rodiganSmoke.jpg',
+        alt: 'rodiganSmoke',
+        colSpan: 6
+    },
+    {
+        path: '/portfolio/music/nozstockCrowd.jpg',
+        alt: 'nozstockCrowd',
+        colSpan: 6
+    },
+    {
+        path: '/portfolio/music/skreamHideout.jpg',
+        alt: 'skreamHideout',
+        colSpan: 6
+    },
+    {
+        path: '/portfolio/music/joyOrbison.jpg',
+        alt: 'joyOrbison',
+        colSpan: 6
+    },
+    {
+        path: '/portfolio/music/andyC.jpg',
+        alt: 'andyC',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/lunice.jpg',
+        alt: 'lunice',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/flava.jpg',
+        alt: 'flava',
+        colSpan: 4,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/stormzy.jpg',
+        alt: 'stormzy',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/jamBaxter.jpg',
+        alt: 'jamBaxter',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/wretch32.jpg',
+        alt: 'wretch32',
+        colSpan: 8,
+    },
+    {
+        path: '/portfolio/music/moat.jpg',
+        alt: 'moat',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/bicep.jpg',
+        alt: 'bicep',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/sgpPaint.jpg',
+        alt: 'sgpPaint',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/dimensions.jpg',
+        alt: 'dimensions',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/ritaDance.jpg',
+        alt: 'ritaDance',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/JoeyBadass.jpg',
+        alt: 'JoeyBadass',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/rudimentalThree.jpg',
+        alt: 'rudimentalThree',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/kelisPortrait.jpg',
+        alt: 'kelisPortrait',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/music/actionBronson.jpg',
+        alt: 'actionBronson',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/oceanWisdom.jpg',
+        alt: 'oceanWisdom',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/jonHopkins.jpg',
+        alt: 'jonHopkins',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/skream.jpg',
+        alt: 'skream',
+        colSpan: 4,
     },
     {
         path: '/portfolio/music/premier.jpg',
-        alt: 'premier'
+        alt: 'premier',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/planB.jpg',
+        alt: 'planB',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/asap.jpg',
+        alt: 'asap',
+        colSpan: 6,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/killerMike.jpg',
+        alt: 'killerMike',
+        colSpan: 6,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/bar.jpg',
+        alt: 'bar',
+        colSpan: 6,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/handshake.jpg',
+        alt: 'handshake',
+        colSpan: 6,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/darius.jpg',
+        alt: 'darius',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/robertPlant.jpg',
+        alt: 'robertPlant',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/soulClap.jpg',
+        alt: 'soulClap',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/music/jrocc.jpg',
+        alt: 'jrocc',
+        colSpan: 8,
+        rowSpan: 2,
+
+    },
+    {
+        path: '/portfolio/music/shoulders.jpg',
+        alt: 'shoulders',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/loveboxCrowd.jpg',
+        alt: 'loveboxCrowd',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/parklifeCrowd.jpg',
+        alt: 'parklifeCrowd',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/paloma.jpg',
+        alt: 'paloma',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/paak.jpg',
+        alt: 'paak',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/kelis.jpg',
+        alt: 'kelis',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/example.jpg',
+        alt: 'example',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/gunshots.jpg',
+        alt: 'gunshots',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/baauer.jpg',
+        alt: 'baauer',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/hands.jpg',
+        alt: 'hands',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/rita.jpg',
+        alt: 'rita',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/music/ellie.jpg',
+        alt: 'ellie',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/jurassic5.jpg',
+        alt: 'jurassic5',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/joey.jpg',
+        alt: 'joey',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/heidi.jpg',
+        alt: 'heidi',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/daniel.jpg',
+        alt: 'daniel',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/dj.jpg',
+        alt: 'dj',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/dangelo.jpg',
+        alt: 'dangelo',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/tinchy.jpg',
+        alt: 'tinchy',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/stormzyUp.jpg',
+        alt: 'stormzyUp',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/damien.jpg',
+        alt: 'damien',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/music/guitar.jpg',
+        alt: 'guitar',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/evolutionCrowd.jpg',
+        alt: 'evolutionCrowd',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/charlie.jpg',
+        alt: 'charlie',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/music/daughter.jpg',
+        alt: 'daughter',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/music/vaccines.jpg',
+        alt: 'vaccines',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/jake.jpg',
+        alt: 'jake',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/music/aluna.jpg',
+        alt: 'aluna',
+        colSpan: 4,
+    },
+
+
+/*
+    {
+        path: '/portfolio/music/nozstockThree.jpg',
+        alt: 'nozstockThree'
     },
     {
         path: '/portfolio/music/purplePortrait.jpg',
         alt: 'purplePortrait'
     },
     {
-        path: '/portfolio/music/rita.jpg',
-        alt: 'rita'
-    },
-    {
-        path: '/portfolio/music/ritaDance.jpg',
-        alt: 'ritaDance'
-    },
-    {
-        path: '/portfolio/music/robertPlant.jpg',
-        alt: 'robertPlant'
-    },
-    {
-        path: '/portfolio/music/rodiganSmoke.jpg',
-        alt: 'rodiganSmoke'
-    },
-    {
-        path: '/portfolio/music/rudimentalThree.jpg',
-        alt: 'rudimentalThree'
-    },
-    {
-        path: '/portfolio/music/sgpPaint.jpg',
-        alt: 'sgpPaint'
-    },
-    {
-        path: '/portfolio/music/shoulders.jpg',
-        alt: 'shoulders'
-    },
-    {
-        path: '/portfolio/music/skream.jpg',
-        alt: 'skream'
-    },
-    {
-        path: '/portfolio/music/skreamHideout.jpg',
-        alt: 'skreamHideout'
-    },
-    {
-        path: '/portfolio/music/soulClap.jpg',
-        alt: 'soulClap'
-    },
-    {
-        path: '/portfolio/music/stormzy.jpg',
-        alt: 'stormzy'
-    },
-    {
-        path: '/portfolio/music/stormzyUp.jpg',
-        alt: 'stormzyUp'
-    },
-    {
         path: '/portfolio/music/submotion.jpg',
         alt: 'submotion'
     },
-    {
-        path: '/portfolio/music/tinchy.jpg',
-        alt: 'tinchy'
-    },
-    {
-        path: '/portfolio/music/vaccines.jpg',
-        alt: 'vaccines'
-    },
-    {
-        path: '/portfolio/music/weAreCrowd.jpg',
-        alt: 'weAreCrowd'
-    },
-    {
-        path: '/portfolio/music/wretch32.jpg',
-        alt: 'wretch32'
-    }
+*/
 ]
 export default photos;
--- src/constants/streetPhotos.js
+++ src/constants/streetPhotos.js
@@ -1,171 +1,227 @@
 const photos = [
     {
-        path: '/portfolio/street/ballers.jpg',
-        alt: 'ballers'
-    },
-    {
-        path: '/portfolio/street/basketball.jpg',
-        alt: 'basketball'
-    },
-    {
-        path: '/portfolio/street/bible.jpg',
-        alt: 'bible'
-    },
-    {
-        path: '/portfolio/street/birds.jpg',
-        alt: 'birds'
-    },
-    {
-        path: '/portfolio/street/block.jpg',
-        alt: 'block'
-    },
-    {
-        path: '/portfolio/street/break.jpg',
-        alt: 'break'
-    },
-    {
-        path: '/portfolio/street/breakfast.jpg',
-        alt: 'breakfast'
-    },
-    {
-        path: '/portfolio/street/building.jpg',
-        alt: 'building'
-    },
-    {
-        path: '/portfolio/street/chinaTown.jpg',
-        alt: 'chinaTown'
-    },
-    {
-        path: '/portfolio/street/commute.jpg',
-        alt: 'commute'
-    },
-    {
-        path: '/portfolio/street/delight.jpg',
-        alt: 'delight'
-    },
-    {
-        path: '/portfolio/street/dosToros.jpg',
-        alt: 'dosToros'
+        path: '/portfolio/street/subwayScream.jpg',
+        alt: 'subwayScream',
+        colSpan: 4,
     },
     {
         path: '/portfolio/street/doubleTake.jpg',
-        alt: 'doubleTake'
-    },
-    {
-        path: '/portfolio/street/duane.jpg',
-        alt: 'duane'
-    },
-    {
-        path: '/portfolio/street/ferry.jpg',
-        alt: 'ferry'
-    },
-    {
-        path: '/portfolio/street/flatiron.jpg',
-        alt: 'flatiron'
-    },
-    {
-        path: '/portfolio/street/gnt.jpg',
-        alt: 'gnt'
-    },
-    {
-        path: '/portfolio/street/green.jpg',
-        alt: 'green'
-    },
-    {
-        path: '/portfolio/street/hat.jpg',
-        alt: 'hat'
-    },
-    {
-        path: '/portfolio/street/information.jpg',
-        alt: 'information'
-    },
-    {
-        path: '/portfolio/street/jewish.jpg',
-        alt: 'jewish'
-    },
-    {
-        path: '/portfolio/street/justMarried.jpg',
-        alt: 'justMarried'
-    },
-    {
-        path: '/portfolio/street/libertyBike.jpg',
-        alt: 'libertyBike'
-    },
-    {
-        path: '/portfolio/street/libertyPhone.jpg',
-        alt: 'libertyPhone'
-    },
-    {
-        path: '/portfolio/street/manhattanBridge.jpg',
-        alt: 'manhattanBridge'
-    },
-    {
-        path: '/portfolio/street/miami.jpg',
-        alt: 'miami'
-    },
-    {
-        path: '/portfolio/street/minettaTavern.jpg',
-        alt: 'minettaTavern'
-    },
-    {
-        path: '/portfolio/street/pigeonGuy.jpg',
-        alt: 'pigeonGuy'
-    },
-    {
-        path: '/portfolio/street/primary.jpg',
-        alt: 'primary'
-    },
-    {
-        path: '/portfolio/street/rabbiRoad.jpg',
-        alt: 'rabbiRoad'
-    },
-    {
-        path: '/portfolio/street/reflected.jpg',
-        alt: 'reflected'
-    },
-    {
-        path: '/portfolio/street/rockawayTowel.jpg',
-        alt: 'rockawayTowel'
-    },
-    {
-        path: '/portfolio/street/screenLight.jpg',
-        alt: 'screenLight'
-    },
-    {
-        path: '/portfolio/street/shelborne.jpg',
-        alt: 'shelborne'
-    },
-    {
-        path: '/portfolio/street/silhouette.jpg',
-        alt: 'silhouette'
-    },
-    {
-        path: '/portfolio/street/stare.jpg',
-        alt: 'stare'
-    },
-    {
-        path: '/portfolio/street/subwayScream.jpg',
-        alt: 'subwayScream'
+        alt: 'doubleTake',
+        colSpan: 4,
     },
     {
         path: '/portfolio/street/tourist.jpg',
-        alt: 'tourist'
+        alt: 'tourist',
+        colSpan: 4,
+        rowSpan: 3
     },
     {
-        path: '/portfolio/street/underground.jpg',
-        alt: 'underground'
+        path: '/portfolio/street/rabbiRoad.jpg',
+        alt: 'rabbiRoad',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/stare.jpg',
+        alt: 'stare',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/ballers.jpg',
+        alt: 'ballers',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/delight.jpg',
+        alt: 'delight',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/ferry.jpg',
+        alt: 'ferry',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/commute.jpg',
+        alt: 'commute',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/bible.jpg',
+        alt: 'bible',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/duane.jpg',
+        alt: 'duane',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/miami.jpg',
+        alt: 'miami',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/dosToros.jpg',
+        alt: 'dosToros',
+        colSpan: 4,
     },
     {
         path: '/portfolio/street/vanessas.jpg',
-        alt: 'vanessas'
+        alt: 'vanessas',
+        colSpan: 4,
     },
     {
-        path: '/portfolio/street/waiting.jpg',
-        alt: 'waiting'
+        path: '/portfolio/street/reflected.jpg',
+        alt: 'reflected',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/birds.jpg',
+        alt: 'birds',
+        colSpan: 8,
+        rowSpan: 2,
     },
     {
         path: '/portfolio/street/walkingAway.jpg',
-        alt: 'walkingAway'
-    }
+        alt: 'walkingAway',
+        colSpan: 7,
+    },
+    {
+        path: '/portfolio/street/libertyBike.jpg',
+        alt: 'libertyBike',
+        colSpan: 5,
+    },
+    {
+        path: '/portfolio/street/shelborne.jpg',
+        alt: 'shelborne',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/justMarried.jpg',
+        alt: 'justMarried',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/primary.jpg',
+        alt: 'primary',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/chinaTown.jpg',
+        alt: 'chinaTown',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/screenLight.jpg',
+        alt: 'screenLight',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/waiting.jpg',
+        alt: 'waiting',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/block.jpg',
+        alt: 'block',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/building.jpg',
+        alt: 'building',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/flatiron.jpg',
+        alt: 'flatiron',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/manhattanBridge.jpg',
+        alt: 'manhattanBridge',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/silhouette.jpg',
+        alt: 'silhouette',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/jewish.jpg',
+        alt: 'jewish',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/break.jpg',
+        alt: 'break',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/basketball.jpg',
+        alt: 'basketball',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/hat.jpg',
+        alt: 'hat',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/green.jpg',
+        alt: 'green',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/minettaTavern.jpg',
+        alt: 'minettaTavern',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/libertyPhone.jpg',
+        alt: 'libertyPhone',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/breakfast.jpg',
+        alt: 'breakfast',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/rockawayTowel.jpg',
+        alt: 'rockawayTowel',
+        colSpan: 4,
+    },
+    {
+        path: '/portfolio/street/information.jpg',
+        alt: 'information',
+        colSpan: 4,
+        rowSpan: 3,
+    },
+    {
+        path: '/portfolio/street/pigeonGuy.jpg',
+        alt: 'pigeonGuy',
+        colSpan: 8,
+        rowSpan: 2,
+    },
+    {
+        path: '/portfolio/street/underground.jpg',
+        alt: 'underground',
+        colSpan: 6,
+    },
+    {
+        path: '/portfolio/street/gnt.jpg',
+        alt: 'gnt',
+        colSpan: 6,
+    },
 ]
 export default photos;
